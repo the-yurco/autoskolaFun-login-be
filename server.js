07e9150d1cad4789ac5304c01ee578507c1478b4
the-yurco <jurajbosy404@gmail.com>
2024-11-18 20:33:15 +0100

    updatiiik1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql2/promise");
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
-const cors = require("cors");
+
 require("dotenv").config();
 const MySQLStore = require("express-mysql-session")(session);
 const { body, validationResult } = require("express-validator");

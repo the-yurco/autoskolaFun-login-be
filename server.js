c14e5133e6b5dc218dd6d738a504c9e38970e2cc
the-yurco <jurajbosy404@gmail.com>
2024-11-18 19:23:09 +0100

    update

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS configuration
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://dev-ucebnicafun.emax-controls.eu/"],
     credentials: true,
   })
 );

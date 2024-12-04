38770667bad944aeac0421b6c2bca6d9be3f826e
the-yurco <jurajbosy404@gmail.com>
2024-12-04 10:50:38 +0100

    just changing it for localhost

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "http://localhost:3000",
-    origin: "https://dev-ucebnicafun.emax-controls.eu",
+    // origin: "https://dev-ucebnicafun.emax-controls.eu",
     credentials: true, // Allow cookies
   })
 );

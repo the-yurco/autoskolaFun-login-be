b1bc5f0f4d25181f40f210814467a2a27aa3695a
the-yurco <jurajbosy404@gmail.com>
2024-12-09 08:17:31 +0100

    localhost

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     //
-    // origin: "http://localhost:3000",
-    origin: "https://dev-ucebnicafun.emax-controls.eu",
+    origin: "http://localhost:3000",
+    // origin: "https://dev-ucebnicafun.emax-controls.eu",
     credentials: true, // Allow cookies
   })
 );
